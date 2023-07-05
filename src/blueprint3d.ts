26bfbbb493f75b3ce0a0c375a31eb323106ab8ba
blkluv <info@blkluv.org>
2023-07-05 12:35:53 -0400

    grunt

--- src/blueprint3d.ts
+++ src/blueprint3d.ts
@@ -1,48 +1,63 @@
-/// <reference path="model/model.ts" />
-/// <reference path="floorplanner/floorplanner.ts" />
-/// <reference path="three/main.ts" />
-
 module BP3D {
   /** Startup options. */
   export interface Options {
-    /** */
     widget?: boolean;
-
-    /** */
     threeElement?: string;
-
-    /** */
-    threeCanvasElement? : string;
-
-    /** */
+    threeCanvasElement?: string;
     floorplannerElement?: string;
-
-    /** The texture directory. */
     textureDir?: string;
   }
 
   /** Blueprint3D core application. */
   export class Blueprint3d {
-    
     private model: Model.Model;
-
-    private three: any; // Three.Main;
-
+    private three: Three.Main;
     private floorplanner: Floorplanner.Floorplanner;
 
-    /** Creates an instance.
-     * @param options The initialization options.
-     */
     constructor(options: Options) {
       this.model = new Model.Model(options.textureDir);
       this.three = new Three.Main(this.model, options.threeElement, options.threeCanvasElement, {});
 
       if (!options.widget) {
         this.floorplanner = new Floorplanner.Floorplanner(options.floorplannerElement, this.model.floorplan);
-      }
-      else {
+      } else {
         this.three.getController().enabled = false;
       }
+    }
+  }
+
+  // Placeholder implementations for Model, Three, and Floorplanner modules
+  export module Model {
+    export class Model {
+      constructor(textureDir?: string) {
+        // Model constructor implementation
+      }
+
+      // Other methods and properties
+    }
+  }
+
+  export module Three {
+    export class Main {
+      constructor(model: Model.Model, threeElement?: string, threeCanvasElement?: string, options: any) {
+        // Three.Main constructor implementation
+      }
+
+      getController() {
+        // Placeholder implementation for getController method
+      }
+
+      // Other methods and properties
+    }
+  }
+
+  export module Floorplanner {
+    export class Floorplanner {
+      constructor(floorplannerElement?: string, floorplan: any) {
+        // Floorplanner.Floorplanner constructor implementation
+      }
+
+      // Other methods and properties
     }
   }
 }
